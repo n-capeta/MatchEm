bd47af228c4b736ce96ad2f3aa039914132563c2
Nathaniel Capeta <capetanathaniel@gmail.com>
2024-09-26 21:05:54 -0400

    Adding start game functionality & a single random rectangle appears

--- MatchEm/GameSceneViewController.swift
+++ MatchEm/GameSceneViewController.swift
@@ -8,11 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print ("View Loaded")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startGame(_ sender: UIButton) {
+        sender.isHidden = true; // Hide button after clicked
+        startGame()
+    }
+    
+    func startGame(){
+        print ("Game started")
+        makeRectPair()
+    }
+    
+    
+    func makeRectPair(){
+        let randWidth  = CGFloat.random(in: 50...150)
+        let randHeight = CGFloat.random(in: 50...150)
+        let randSize   = CGSize(width: randWidth, height: randHeight)
+        let randColor  = UIColor(
+            red:   CGFloat.random(in: 0...1),
+            green: CGFloat.random(in: 0...1),
+            blue:  CGFloat.random(in: 0...1),
+            alpha: 1.0
+        )
+        
+        let rect  = UIView()
+        rect.frame.size      = randSize
+        rect.backgroundColor = randColor
+        let x = CGFloat.random(in: self.view.frame.minX...(self.view.frame.maxX - randWidth))
+        let y = CGFloat.random(in: self.view.safeAreaInsets.top...(self.view.frame.maxY - randHeight - self.view.safeAreaInsets.bottom))
+        
+        rect.frame.origin = CGPoint(x: x, y: y)
+        view.addSubview(rect)
+    }
 }
